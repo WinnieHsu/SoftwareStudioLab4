bf46ae4684a8668a9e29fef20aa31d37fcf51022
WinnieHsu <s101021122@m101.nthu.edu.tw>
2016-03-24 18:38:49 +0800

    <modify MyJPanel.java>

--- src/MyJPanel.java
+++ src/MyJPanel.java
@@ -1,4 +1,3 @@
-
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
@@ -9,22 +8,24 @@ import javax.swing.*;
 public class MyJPanel extends JPanel implements MouseMotionListener{
 	
 	private Square square = new Square(100);
+	int centerX = 0;
+	int centerY = 0;
 	
 	public MyJPanel(){
 		addMouseMotionListener(this);
 	}
 	protected void paintComponent(Graphics g){
 		super.paintComponent(g);
+		g.setColor(square.getRandomColor());
 		// TODO Draw square and fill it with random color decided by method getRandomColor()
-		// You should use fillRect()
-		
+		g.fillRect(centerX, centerY, square.getShapeHeight(), square.getShapeWidth());
 	}
 
 	@Override
 	public void mouseDragged(MouseEvent e) {
 		// TODO Get mouse dragged position and change suqare's position
-		centerX = e.getX();
-		centerY = e.getY();
+		centerX = e.getX()-square.getShapeWidth()/2;
+		centerY = e.getY()-square.getShapeHeight()/2;
 		repaint();
 	}
 
